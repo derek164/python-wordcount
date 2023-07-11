6e4abb5959da23104b0abae60978d949795419e1
Derek Zhang <derek.zhang@C02FG1ZMQ05N-Maven-Wave.local>
2023-07-11 10:02:52 +0800

    fast agg

--- count.py
+++ count.py
@@ -1,14 +1,28 @@
 import json
 import time
 from collections import Counter
-from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
-from functools import reduce
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from functools import wraps
 from itertools import zip_longest
 from multiprocessing import cpu_count
 from operator import add
 from pathlib import Path
 
 root = Path(__file__).parent
+
+
+def timeit(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        elapsed = format(end_time - start_time, ".4f")
+        template = "Function: {name:<30} Time (s): {elapsed:<10}"
+        print(template.format(name=func.__name__, elapsed=elapsed))
+        return result
+
+    return timeit_wrapper
 
 
 class WordCount:
@@ -24,12 +38,8 @@ class WordCount:
     def __exit__(self, exc_type, exc_value, traceback):
         [f.unlink() for f in self.temp.glob("*")]
 
-    def execute(self):
-        self.split()
-        counts = self.count_partitions()
-        return self.collect(counts)
-
-    def split(self):
+    @timeit
+    def split_file(self):
         def grouper(n, iterable, fillvalue=None):
             args = [iter(iterable)] * n
             return zip_longest(fillvalue=fillvalue, *args)
@@ -39,25 +49,23 @@ class WordCount:
                 with open(self.temp / f"partition_{i}.txt", "w") as fout:
                     fout.writelines(g)
 
+    @timeit
     def count_partitions(self):
+        counter = Counter()
         with ProcessPoolExecutor(max_workers=cpu_count()) as executor:
             futures = [
                 executor.submit(self.count_partition, file)
                 for file in self.temp.glob("*")
             ]
 
-            counts = []
             for future in as_completed(futures):
-                result = future.result()
-                counts.append(result)
+                partition_count = future.result()
+                counter += partition_count
 
-            return counts
+            return dict(counter.most_common())
 
     def count_partition(self, file):
         return Partition(file).count()
-
-    def collect(self, counts):
-        return dict(reduce(add, map(Counter, counts)).most_common())
 
 
 class Partition:
@@ -68,36 +76,24 @@ class Partition:
         return Counter(line.split())
 
     def count(self):
-        # counter = Counter()
-        # with open(self.file) as f:
-        #     for line in f:
-        #         counter += self.line_count(line)
-        # return counter
-
         counter = Counter()
-        with ThreadPoolExecutor(max_workers=10) as executor:
-            futures = []
-            with open(self.file) as f:
-                for line in f:
-                    futures.append(executor.submit(self.line_count, line))
-
-            for future in as_completed(futures):
-                result = future.result()
-                counter += result
-
-            return counter
+        with open(self.file) as f:
+            for line in f:
+                counter += self.line_count(line)
+        return counter
 
 
-def save_count(count, file):
-    with open(file, "w") as f:
-        json.dump(count, f, indent=4)
+@timeit
+def word_count(in_file, out_file):
+    def save_count(count, out_file):
+        with open(out_file, "w") as f:
+            json.dump(count, f, indent=4)
+
+    with WordCount(in_file) as wc:
+        wc.split_file()
+        count = wc.count_partitions()
+        save_count(count, out_file)
 
 
 if __name__ == "__main__":
-    ts = time.time()
-    input_file = "data/big.txt"
-    with WordCount(input_file) as counter:
-        count = counter.execute()
-    # print(count)
-    print(time.time() - ts)
-    save_count(count, "count.json")
+    word_count(in_file="data/big.txt", out_file="count.json")
